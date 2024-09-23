4ba109406b85260438f72d1046745b40f62b81a5
Svel26 <sjjansen06@gmail.com>
2024-09-23 15:14:42 +0200

    Update FileHost.py

--- FileHost.py
+++ FileHost.py
@@ -7,8 +7,8 @@ class MyHandler(SimpleHTTPRequestHandler):
         super().end_headers()
 
 if __name__ == "__main__":
-    os.chdir('Logs/CleanedLogs')  # Change directory to where cleaned logs are stored
-    server_address = ('', 8000)  # Serve on localhost:8000
+    os.chdir('.')
+    server_address = ('', 8000)
     httpd = HTTPServer(server_address, MyHandler)
     print("Serving at http://localhost:8000")
     httpd.serve_forever()
